6ae96ef8f67c2c0649337580fc81f9bdc7919b73
zerathulous@hotmail.com <zerathulous@hotmail.com@d1990c98-db8e-11de-b12c-574f4db5f7e4>
2009-12-19 19:40:03 +0000

    Ajout de la librairie AviFiles

--- trunk/sources/VideoReccorder/Program.cs
+++ trunk/sources/VideoReccorder/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
+using User.DirectShow;
+using AviFile;
+using System.Drawing;
 
 namespace VideoReccorder
 {
-    static class Program
+    public class Program
     {
+        private FrameGrabber fg; //Pour générer un tableau de bitmap
+        private AviManager aviManager;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+        }
+      
+        public void StartRecording(string filename)
+        {
+            Bitmap bitmap = (Bitmap)Image.FromFile(filename);
+
+            //TODO : lire par MMF le nom du fichier de sorti
+            //TODO : lire par MMF les fichier bitmap modifié
+            //bitmap.loadFromStream
+            aviManager = new AviManager(filename + ".avi", false);
+            VideoStream aviStream =  aviManager.AddVideoStream(true, 2, bitmap); //bitmap étant la première image, elle sert a sizer le format du vidéo de sorti
+          
+            int count = 0;
+            
+           
+            //On doit lire toutes les images passé par mmf, et les ajouté au stream
+            //for (int n = 1; n < txtFileNames.Lines.Length; n++)
+            //{
+               // if (txtFileNames.Lines[n].Trim().Length > 0)
+                //{
+                  //  bitmap = (Bitmap)Bitmap.FromFile(txtFileNames.Lines[n]);
+                    aviStream.AddFrame(bitmap);
+                    bitmap.Dispose();
+                    //count++;
+               // }
+            //}
+            aviStream.Close();
+            aviManager.Close();
+        }
+
+        public void StoptRecording()
+        {
+            
         }
     }
 }
