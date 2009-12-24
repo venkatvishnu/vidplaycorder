86c7ad1e89bee1704aeeaedc067bf7343b78447f
gagnonthomas <gagnonthomas@gmail.com>
2009-12-24 16:59:03 +0000

    Finalisation de l'installateur Mise à jour de l'architecture

--- sources/VideoReccorder/Properties/AssemblyInfo.cs
+++ sources/VideoReccorder/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VideoPlayer")]
+[assembly: AssemblyTitle("VideoRecorder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Maison")]
 [assembly: AssemblyProduct("Vidplaycorder")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCopyright("Copyright © Maison 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6e1ab4b6-e331-4768-a306-6351512b3d13")]
+[assembly: Guid("4989836a-fcc7-4fa8-a760-af03bfddaf24")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
